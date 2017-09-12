783807a2b06f994865547e2be421f407758ed513
jelliotartz <jelliotartz@gmail.com>
2017-09-12 18:16:34 -0500

    WIP: add external links.

--- src/modules/organizations/containers/details-form-container.jsx
+++ src/modules/organizations/containers/details-form-container.jsx
@@ -10,6 +10,32 @@ import bindInput from '../../common/containers/bind-input';
 import FormContainer from '../../common/containers/form-container';
 import CharLimit from '../../common/components/char-limit';
 
+// import only the clone deep method?
+import _ from 'lodash';
+import Box from 'grommet/components/Box';
+
+const ExternalLinkRow = ({ link, onLabelChange, onUrlChange, index }) =>
+  <Box direction="row">
+    <Box>
+      <input
+        type="text"
+        name={link.label}
+        value={link.label}
+        onChange={onLabelChange}
+      />
+    </Box>
+    <Box>
+      <input
+        type="text"
+        name={link.url}
+        value={link.url}
+        onChange={onUrlChange}
+      />
+    </Box>
+  </Box>
+
+
+
 class DetailsFormContainer extends React.Component {
   constructor(props) {
     super(props);
@@ -19,8 +45,12 @@ class DetailsFormContainer extends React.Component {
     this.handleTextAreaChange = this.handleTextAreaChange.bind(this);
     this.resetOrganization = this.resetOrganization.bind(this);
 
+    this.handleUrlChange = this.handleUrlChange.bind(this);
+    this.handleLabelChange = this.handleLabelChange.bind(this);
+
     this.state = {
-      textarea: ''
+      textarea: '',
+      links: this.props.organization.urls
     };
   }
 
@@ -28,9 +58,22 @@ class DetailsFormContainer extends React.Component {
     this.setState({ textarea: this.props.organization.introduction });
   }
 
+  handleUrlChange(event, index) {
+    // create copy of this state links
+    const linksCopy = _.cloneDeep(this.state.links);
+    linksCopy[index].url = event.target.value; // this mutates state. bad!
+    this.setState({ links: linksCopy });
+  }
+
+  handleLabelChange(event, index) {
+    // create copy of this state links
+    const linksCopy = _.cloneDeep(this.state.links)
+    linksCopy[index].label = event.target.value; // this mutates state. bad!
+    this.setState({ links: linksCopy });
+  }
+
   handleTextAreaChange(event) {
     const textarea = event.target.value;
-
     this.setState({ textarea });
   }
 
@@ -42,6 +85,8 @@ class DetailsFormContainer extends React.Component {
       result[fieldName] = this.fields[fieldName].value();
     });
     result.introduction = this.state.textarea;
+    result.urls = this.state.links;
+
     return result;
   }
 
@@ -107,11 +152,38 @@ class DetailsFormContainer extends React.Component {
               <CharLimit limit={1500} string={this.state.textarea || ''} />
             </small>
           </fieldset>
+
+          <fieldset className="form__fieldset">
+            <label className="form__label">
+              External Links
+              <Box size={{width: {max: 'medium'}}}>
+                <Box direction="row" margin={{top: 'small'}}>
+                  <Box style={{minWidth: '110px', textAlign: 'center'}}>Label</Box>
+                  <Box style={{minWidth: '110px', textAlign: 'center'}}>Url</Box>
+                </Box>
+                {this.state.links && this.state.links.map((link, index) =>
+                  <ExternalLinkRow
+                    key={`external-link-${index}`}
+                    link={link}
+                    index={index}
+                    onLabelChange={(event) => this.handleLabelChange(event, index)}
+                    onUrlChange={(event) => this.handleUrlChange(event, index)}
+                  />
+                )}
+              </Box>
+            </label>
+            <small className="form__help">
+              Adding an external link will make it appear as a new tab alongside
+              the about, classify, talk, and collect tabs.
+            </small>
+          </fieldset>
+
         </FormContainer>
       </div>
     );
   }
 }
+
 
 DetailsFormContainer.defaultProps = {
   organization: {},
